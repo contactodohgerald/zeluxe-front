8391003a62f6f8baba0ae124a8151fbe8018f942
samcesa45 <samcesa45@gmail.com>
2024-11-27 17:08:49 +0100

    minor fix

--- src/config/env.ts
+++ src/config/env.ts
@@ -11,7 +11,7 @@ const createEnv = () => {
     APP_URL: z.string().optional().default('https://api.zeluxe.ng'),
     APP_MOCK_API_PORT: z.string().optional().default('8080'),
   });
-  const envVars = Object.entries(import.meta.env.BASE_URL).reduce<
+  const envVars = Object.entries(import.meta.env).reduce<
     Record<string, string>
   >((acc, curr) => {
     const [key, value] = curr;
